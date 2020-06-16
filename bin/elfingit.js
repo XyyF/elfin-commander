1a7a283f4c78d41f12883566f86eec819e702caa
rengar <915724526@qq.com>
2020-06-17 07:08:23 +0800

    feat(master): 命令结构

--- bin/elfingit.js
+++ bin/elfingit.js
@@ -7,14 +7,40 @@ program
     .usage('<command> [options]')
 
 program
-    .command('depend <depend name>')
-    .description('初始化依赖工程')
-    .on('--help', () => {
-        console.log('')
-        console.log('Usage: elfingit depend')
-        console.log('Options:')
-        console.log('  init')
-        console.log('  install')
+    .command('clone <source>')
+    .description('clone a repository into a newly created directory')
+    .action((source, destination) => {
+            console.log('clone command called', source, destination)
     })
+
+program
+    .command('init')
+    .description('init depend repository')
+    .action(() => {
+            console.log('clone command called')
+    })
+
+program
+    .command('install [name]')
+    .description('depend repository run npm install')
+    .action((name) => {
+            console.log('clone command called', name)
+    })
+
+program
+    .command('hooks <hookName> [options]')
+    .description('depend repository run npm install')
+    .action((hookName, options) => {
+            console.log('clone command called', hookName, options)
+    })
+    .on('--help', function() {
+            console.log('');
+            console.log('hook-name:');
+            console.log('  post-checkout');
+            console.log('  pre-push');
+            console.log('  post-merge');
+            console.log('  reset-branch');
+            console.log('');
+    });
 
 program.parse(process.argv)
