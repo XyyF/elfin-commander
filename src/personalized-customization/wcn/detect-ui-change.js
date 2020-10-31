e355a9a39ee07a839bd69e57d5d6dc0cb823f52c
rengarxiao <rengarxiao@tencent.com>
2020-10-31 14:47:25 +0800

    feat: 流程规范

--- src/personalized-customization/wcn/detect-ui-change.js
+++ src/personalized-customization/wcn/detect-ui-change.js
@@ -1,4 +1,5 @@
 const shell = require('shelljs');
+const path = require('path');
 const execSync = require('child_process').execSync; //同步子进程
 const ShellUtil = require('../../../utils/shell');
 const { wcxUIDetect } = require('../../../utils/enums');
@@ -7,6 +8,7 @@ const { validateUIProxy } = require('../../../utils/config-vlidator');
 
 // 检测UI变动
 module.exports = async function detectUIChange() {
+    logUtil.log('elfincmd pc detectUI start');
     // 获取到配置文件
     const shellUtil = new ShellUtil();
     /**
@@ -20,15 +22,27 @@ module.exports = async function detectUIChange() {
     validateUIProxy(config);
     // 进入到UI仓库
     shell.cd(config.uiRepoAddress);
+    logUtil.log(`进入UI工程目录: 【${path.resolve(process.cwd(), './')}】`);
 
-    const diffStr = execSync('git diff --stat master origin/master').toString().trim();
+    const branch = config.branch || 'master'
+    const diffStr = execSync(`git diff --stat ${branch} origin/${branch}`).toString().trim();
     const diffArr = diffStr.match(/dist.*(.json|.wxml|.wxss|.js)/g);
+    logUtil.log(`获取 【${branch}】 分支文件diff`);
 
-    logUtil.log('以下文件存在变动，请及时同步')
+    let related = '';
+    let unRelated = '';
     for (let item of diffArr) {
-        const proxy = config.proxy.find(e => e.source === item)
+        const proxy = config.proxy.find(e => e.source === item);
         if (proxy) {
-            logUtil.logGreen(`业务文件: ${proxy.target} | UI文件: ${config.uiRepoAddress}/${proxy.source}`);
+            related += `业务文件: ${proxy.target} 【${proxy.desc}】| UI文件: ${config.uiRepoAddress}/${item}\n`;
+        } else {
+            unRelated += `UI文件: ${config.uiRepoAddress}/${item} \n`;
         }
     }
+    logUtil.log('以下文件存在变动，请及时同步');
+    logUtil.logGreen(related.trim());
+    logUtil.log('以下文件存在变动,但未建立关联，请及时同步');
+    logUtil.logGreen(unRelated.trim());
+
+    logUtil.log('elfincmd pc detectUI end');
 };
