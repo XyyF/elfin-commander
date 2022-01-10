1fe72b63e786771e81119594d3e05c1065982f20
rengarxiao <915724526@qq.com>
2022-01-10 19:11:49 +0800

    feat: gulp copy templates

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var child_process = require('child_process');
+
+function copyDir(src, dist) {
+  child_process.spawn('cp', ['-r', src, dist]);	
+}
+
+function defaultTask(done) {
+  copyDir('./templates', './lib/templates');
+  done();
+}
+
+exports.default = defaultTask
